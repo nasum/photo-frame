150ecd292dc07125cb531c2f80442e5d76c9e937
nasum <tomato.wonder.life@gmail.com>
2017-07-22 10:14:12 +0900

    init vue and photon

---
+++ src/renderer/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="window">
+    hogehoge
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style src="css/base.scss"></style>
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const webpack = require('webpack')
 const path = require('path')
 const ExternalsPlugin = webpack.ExternalsPlugin
@@ -6,7 +7,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
   entry: {
     "browser/app": './src/browser/app.js',
-    "renderer/index": './src/renderer/index.jsx'
+    "renderer/index": './src/renderer/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'build'),
@@ -15,7 +16,11 @@ module.exports = {
   target: "node",
   devtool: 'source-map',
   resolve: {
-    extensions: ['.webpack.js', '.web.js', '.js', '.jsx', '.scss']
+    extensions: ['.webpack.js', '.web.js', '.js', '.vue', '.css', '.scss'],
+    alias: {
+      'vue': 'vue/dist/vue',
+    },
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
   },
   module: {
     rules: [
@@ -26,9 +31,59 @@ module.exports = {
         loader: "eslint-loader",
       },
       {
-        test: /\.js[x]?$/,
+        test: /\.vue?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            css: ExtractTextPlugin.extract({
+              use: 'css-loader',
+              fallback: 'vue-style-loader'
+            }),
+            scss: ExtractTextPlugin.extract({
+              use: 'css-loader!sass-loader',
+              fallback: 'vue-style-loader'
+            }),
+            sass: ExtractTextPlugin.extract({
+              use: 'css-loader!sass-loader?indentedSyntax',
+              fallback: 'vue-style-loader'
+            })
+          },
+          esModule: true,
+          optimizeSSR: false
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader",
+          publicPath: "./build"
+        })
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader!sass-loader",
+          publicPath: "./build"
+        })
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?limit=10000&mimetype=application/font-woff&name=[hash].[ext]&outputPath=fonts/&publicPath=../"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?limit=10000&mimetype=application/octet-stream&name=[hash].[ext]&outputPath=fonts/&publicPath=../"
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?limit=10000&mimetype=application/octet-stream&name=[hash].[ext]&outputPath=fonts/&publicPath=../"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader?limit=10000&mimetype=image/svg+xml&name=[hash].[ext]&outputPath=fonts/&publicPath=../"
       }
     ]
   },
@@ -42,6 +97,7 @@ module.exports = {
     new CopyWebpackPlugin([
       { from: 'src/index.html', to: 'index.html' },
       { from: 'src/package.json', to: 'package.json' }
-    ])
+    ]),
+    new ExtractTextPlugin("css/style.css")
   ]
 }
